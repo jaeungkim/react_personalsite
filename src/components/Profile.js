ba4ffd7c8fd766a396914726a3fb1085d6bcbcba
JaeKim <khyy37@hotmail.com>
2020-05-18 18:19:44 -0700

    minor changes on home
    minor changes on home

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -12,28 +12,121 @@ class Profile extends Component {
             params={{
               "particles": {
                 "number": {
-                  "value": 100
+                  "value": 355,
+                  "density": {
+                    "enable": true,
+                    "value_area": 789.1476416322727
+                  }
+                },
+                "color": {
+                  "value": "#ffffff"
+                },
+                "shape": {
+                  "type": "circle",
+                  "stroke": {
+                    "width": 0,
+                    "color": "#000000"
+                  },
+                  "polygon": {
+                    "nb_sides": 5
+                  },
+                  "image": {
+                    "src": "img/github.svg",
+                    "width": 100,
+                    "height": 100
+                  }
+                },
+                "opacity": {
+                  "value": 0.48927153781200905,
+                  "random": false,
+                  "anim": {
+                    "enable": true,
+                    "speed": 0.2,
+                    "opacity_min": 0,
+                    "sync": false
+                  }
                 },
                 "size": {
-                  "value": 5
+                  "value": 2,
+                  "random": true,
+                  "anim": {
+                    "enable": true,
+                    "speed": 2,
+                    "size_min": 0,
+                    "sync": false
+                  }
+                },
+                "line_linked": {
+                  "enable": false,
+                  "distance": 150,
+                  "color": "#ffffff",
+                  "opacity": 0.4,
+                  "width": 1
+                },
+                "move": {
+                  "enable": true,
+                  "speed": 0.2,
+                  "direction": "none",
+                  "random": true,
+                  "straight": false,
+                  "out_mode": "out",
+                  "bounce": false,
+                  "attract": {
+                    "enable": false,
+                    "rotateX": 600,
+                    "rotateY": 1200
+                  }
                 }
               },
               "interactivity": {
+                "detect_on": "canvas",
                 "events": {
                   "onhover": {
                     "enable": true,
-                    "mode": "repulse"
+                    "mode": "bubble"
+                  },
+                  "onclick": {
+                    "enable": true,
+                    "mode": "push"
+                  },
+                  "resize": true
+                },
+                "modes": {
+                  "grab": {
+                    "distance": 400,
+                    "line_linked": {
+                      "opacity": 1
+                    }
+                  },
+                  "bubble": {
+                    "distance": 83.91608391608392,
+                    "size": 1,
+                    "duration": 3,
+                    "opacity": 1,
+                    "speed": 3
+                  },
+                  "repulse": {
+                    "distance": 200,
+                    "duration": 0.4
+                  },
+                  "push": {
+                    "particles_nb": 4
+                  },
+                  "remove": {
+                    "particles_nb": 2
                   }
                 }
-              }
-            }} />
+              },
+              "retina_detect": true
+            }
+            } />
           <div class="home-table">
             <div class="home-table-center">
               <div class="container">
                 <div class="row justify-content-center">
                   <div class="col-lg-12">
                     <div class="text-white text-center">
-                      <h1 class="header_title mb-0 mt-3"> I am{' '}
+                      <h1 class="header_title mb-0 mt-3"> I am <span>
                         <Typical
                           loop={Infinity}
                           wrapper='b'
@@ -43,7 +136,7 @@ class Profile extends Component {
                             'a Designer.', 4000,
                             'a Blogger.', 4000
                           ]}
-                        />
+                        /></span>
                       </h1>
                     </div>
                   </div>
