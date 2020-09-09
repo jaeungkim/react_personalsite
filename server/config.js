3508cd31469b3f419872fdef37e063b66ea3db41
JaeKim <khyy37@hotmail.com>
2020-09-08 17:42:01 -0700

    Update config.js

--- server/config.js
+++ server/config.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 var transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
+  host: "smtp.googlemail.com",
   port: 465,
   secure: true,
   auth: {
