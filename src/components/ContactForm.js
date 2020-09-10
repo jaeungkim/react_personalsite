a2f97bbce5b9ebfd6effc9a24db260dbd7444c5b
JaeKim <khyy37@hotmail.com>
2020-09-09 21:46:21 -0700

    contact me update

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,103 +1,62 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 import { Form, Button, Container, Row } from 'react-bootstrap';
 import axios from 'axios';
 
-const ContactForm = () => {
-  const [state, setState] = useState({
-    name: '',
-    email: '',
-    subject: '',
-    message: ''
-  });
+class ContactForm extends Component {
+  constructor(props) {
+    super(props);
+    this.submitForm = this.submitForm.bind(this);
+    this.state = {
+      status: ""
+    };
+  }
 
-  const [result, setResult] = useState(null);
-
-  const sendEmail = event => {
-    event.preventDefault();
-    axios
-      .post('/send', { ...state })
-      .then(response => {
-        setResult(response.data);
-        setState({ name: '', email: '', subject: '', message: '' });
-      })
-      .catch(() => {
-        setResult({ success: false, message: 'Something went wrong. Try again later'});
-      });
-  };
-
-  const onInputChange = event => {
-    const { name, value } = event.target;
-
-    setState({
-      ...state,
-      [name]: value
-    });
-  };
-
-  return (
-    <section className="custom_contact" id="contact">
-      <Container>
-        <div className="text-center mt-3">
-          <h1> Contact Me</h1>
-        </div>
-        <Row>
-          <div className="col-md-6 offset-md-3">
-            {result && (
-              <p className={`${result.success ? 'success' : 'error'}`}>
-                {result.message}
-              </p>
-            )}
-            <form onSubmit={sendEmail}>
-              <Form.Group controlId="name">
-                <Form.Label>Full Name</Form.Label>
-                <Form.Control
-                  type="text"
-                  name="name"
-                  value={state.name}
-                  placeholder="Enter your full name"
-                  onChange={onInputChange}
-                />
-              </Form.Group>
-              <Form.Group controlId="email">
-                <Form.Label>Email</Form.Label>
-                <Form.Control
-                  type="email"
-                  name="email"
-                  value={state.email}
-                  placeholder="Enter your email"
-                  onChange={onInputChange}
-                />
-              </Form.Group>
-              <Form.Group controlId="subject">
-                <Form.Label>Subject</Form.Label>
-                <Form.Control
-                  type="text"
-                  name="subject"
-                  value={state.subject}
-                  placeholder="Enter subject"
-                  onChange={onInputChange}
-                />
-              </Form.Group>
-              <Form.Group controlId="subject">
-                <Form.Label>Message</Form.Label>
-                <Form.Control
-                  as="textarea"
-                  name="message"
-                  value={state.message}
-                  rows="3"
-                  placeholder="Enter your message"
-                  onChange={onInputChange}
-                />
-              </Form.Group>
-              <Button variant="primary" type="submit">
-                Submit
-              </Button>
-            </form>
+  render() {
+    const { status } = this.state;
+    return (
+      <section className="custom_contact" id="contact">
+        <Container>
+          <div className="text-center mt-3">
+            <h1> Contact Me</h1>
           </div>
-        </Row>
-      </Container>
-    </section>
-  );
-};
+          <Row>
+            <form
+              onSubmit={this.submitForm}
+              action="https://formspree.io/xayprage"
+              method="POST"
+            >
+
+              <label>Email:</label>
+              <input type="email" name="email" />
+              <label>Message:</label>
+              <input type="text" name="message" />
+              {status === "SUCCESS" ? <p>Thanks!</p> : <button>Submit</button>}
+              {status === "ERROR" && <p>Ooops! There was an error.</p>}
+            </form>
+          </Row>
+        </Container>
+      </section>
+    );
+  }
+  submitForm(ev) {
+    ev.preventDefault();
+    const form = ev.target;
+    const data = new FormData(form);
+    const xhr = new XMLHttpRequest();
+    xhr.open(form.method, form.action);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        form.reset();
+        this.setState({ status: "SUCCESS" });
+      } else {
+        this.setState({ status: "ERROR" });
+      }
+    };
+    xhr.send(data);
+  }
+}
+
 
 export default ContactForm;
